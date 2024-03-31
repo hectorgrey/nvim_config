e669d84542b4020c1e5e928fc621776f9dddb5c6
Chris Parker <hectorgrey223@protonmail.com>
2024-03-31 17:39:24 +0100

    got lsps working

--- lua/user/plugins/nvim-lspconfig.lua
+++ lua/user/plugins/nvim-lspconfig.lua
@@ -1,3 +1,92 @@
 return {
 	"neovim/nvim-lspconfig",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{
+			"antosha417/nvim-lsp-file-operations",
+			config = true,
+		},
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local keymap = vim.keymap
+		local opts = { noremap = true, silent = true }
+		local on_attach = function(client, buffer)
+			opts.buffer = buffer
+
+			opts.desc = "Show LSP References"
+			keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+			opts.desc = "Go to Definition"
+			keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+			opts.desc = "Show LSP Definitions"
+			keymap.set("n", "gd", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+			opts.desc = "Show LSP Implementations"
+			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+			opts.desc = "Show LSP Type Definitions"
+			keymap.set("n", "gi", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+			opts.desc = "See Available Code Actions"
+			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+			opts.desc = "Smart Rename"
+			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+			opts.desc = "Show Buffer Diagnostics"
+			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+			opts.desc = "Show Line Diagnostics"
+			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+			opts.desc = "Goto Previous Diagnostic"
+			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+			opts.desc = "Goto Next Diagnostic"
+			keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+			opts.desc = "Show Documentation"
+			keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+			opts.desc = "Restart LSP"
+			keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
+		end
+
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		lspconfig["ols"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["rust-analyzer"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
+	end,
 }
