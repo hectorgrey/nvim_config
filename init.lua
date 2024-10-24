b0020e2bd16540f6563dd079c66ba40cee02131d
hectorgrey <hectorgrey223@protonmail.com>
2024-10-24 20:09:17 +0100

    separated options into a new file

--- init.lua
+++ init.lua
@@ -1,23 +1,2 @@
-local opts = vim.opt
+require('options')
 
-opts.backup = true
-opts.backupdir = "backup//"
-opts.breakindent = true
-opts.cdhome = true
-opts.clipboard = "unnamedplus"
-opts.completeopt = "menu,popup,noselect"
-opts.confirm = true
-opts.joinspaces = true
-opts.list = true
-opts.number = true
-opts.relativenumber = true
-opts.scrolloff = 5
-opts.smartcase = true
-opts.smartindent = true
-opts.splitbelow = true
-opts.splitright = true
-opts.tabstop = 4
-opts.undofile = true
-
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
